4935f5506eb3a9c201262715ec120d056ea3a9d6
Benjie Gillam <benjie@jemjie.com>
2018-01-14 11:56:12 +0000

    Fix invalid statement

--- tests/general.test.js
+++ tests/general.test.js
@@ -14,7 +14,7 @@ check(`select 1;
 select 2; create function foo() returns text as $$ select 'hi'; $$ language sql;`);
 check(`select 1;`);
 check("sEleCt 1;");
-check("sEleCt 1 from table;");
+check("sEleCt 1 from my_table;");
 check(`sEleCt 1 from "table";`);
 check(`sEleCt 1 from "Table";`);
 check(`sEleCt     1     as       one     from     "Table";`);
